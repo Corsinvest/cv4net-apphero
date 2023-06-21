7973d57d1660a7dbe11378efdfeec4bfd6f93d50
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2023-06-21 15:47:42 +0200

    Fix webhook

---
+++ src/Corsinvest.AppHero.Core/Notification/WebHook/BodyType.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * SPDX-FileCopyrightText: Copyright Corsinvest Srl
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace Corsinvest.AppHero.Core.Notification.WebHook;
+
+public enum BodyType
+{
+    FormData,
+    Text,
+    Json,
+}
--- src/Corsinvest.AppHero.Core/Notification/WebHook/ChannelOptions.cs
+++ src/Corsinvest.AppHero.Core/Notification/WebHook/ChannelOptions.cs
@@ -5,6 +5,7 @@
 using Corsinvest.AppHero.Core.Domain.Models;
 using Newtonsoft.Json;
 using System.Net;
+using System.Net.Mime;
 
 namespace Corsinvest.AppHero.Core.Notification.WebHook;
 
@@ -13,16 +14,32 @@ public class ChannelOptions : NotificationChannelOptions
     public Credential Credential { get; set; } = new Credential();
 
     [Required]
-    public HttpMethod HttpMethod { get; set; } = default!;
+    public HttpMethod HttpMethod { get; set; } = HttpMethod.Post;
+
+    [Required]
+    public BodyType BodyType { get; set; } = BodyType.FormData;
 
     public override string Type { get; } = "WebHook";
 
-    [Display(Name = "Webhook Url")]
     [Required]
+    [Display(Name = "Webhook Url")]
     public string WebHookUrl { get; set; } = default!;
 
     [JsonIgnore]
     public override string Info => $"{WebHookUrl}";
+
+    [Required]
+    [DataType(DataType.MultilineText)]
+    public string Model { get; set; } = """
+        {
+            "Subject": "{Subject}",
+            "ColorSeverity": "{ColorSeverity}",
+            "ImageSeverity": "{ImageSeverity}",
+            "Body": "{Body}",
+            "Context": "{Context}",
+            "Data": "{Data}"
+        }
+        """;
 
     protected override async Task SendImplAsync(NotificationMessage message)
     {
@@ -33,21 +50,27 @@ public class ChannelOptions : NotificationChannelOptions
         }
         using var client = new HttpClient(handler);
 
-        var dic = new Dictionary<string, string>()
-        {
-            ["Subject"] = message.Subject,
-            ["Body"] = message.Body,
-            ["Context"] = message.Context,
-            ["Severity"] = message.Severity.ToString(),
-        };
-        dic.AddRange(message.Data.ToDictionary(a => $"Data_{a.Key}", a => a.Value.ToString()!));
+        var data = Model.Replace("{Subject}", message.Subject)
+                        .Replace("{ColorSeverity}", message.ColorSeverity)
+                        .Replace("{ImageSeverity}", GetSeverityIcon(message.Severity))
+                        .Replace("{Body}", message.Body)
+                        .Replace("{Context}", message.Context)
+                        .Replace("{Data}", message.Data.Select(a => $"{{\"name\": \"{a.Key}\",\"value\": \"{a.Value}\"}},")
+                                                               .JoinAsString(""));
 
-        var content = new FormUrlEncodedContent(dic);
+        var content = BodyType switch
+        {
+            BodyType.FormData => (ByteArrayContent)new FormUrlEncodedContent(JsonConvert.DeserializeObject<Dictionary<string, string>>(data)!),
+            BodyType.Text => new StringContent(data, System.Text.Encoding.UTF8),
+            BodyType.Json => new StringContent(data, System.Text.Encoding.UTF8, MediaTypeNames.Application.Json),
+            _ => throw new ArgumentOutOfRangeException(nameof(BodyType)),
+        };
 
         var ret = HttpMethod switch
         {
             HttpMethod.Put => await client.PutAsync(WebHookUrl, content),
-            HttpMethod.Post or _ => await client.PostAsync(WebHookUrl, content),
+            HttpMethod.Post => await client.PostAsync(WebHookUrl, content),
+            _ => throw new ArgumentOutOfRangeException(nameof(HttpMethod)),
         };
         if (!ret.IsSuccessStatusCode) { throw new AppHeroException($"Error: {ret.ReasonPhrase}"); }
 
