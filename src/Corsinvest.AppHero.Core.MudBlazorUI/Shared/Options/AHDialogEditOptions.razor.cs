c27c9d1a6407edf8183b060e1ea803e692495e7b
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2023-06-21 15:48:20 +0200

    Save options wating

--- src/Corsinvest.AppHero.Core.MudBlazorUI/Shared/Options/AHDialogEditOptions.razor.cs
+++ src/Corsinvest.AppHero.Core.MudBlazorUI/Shared/Options/AHDialogEditOptions.razor.cs
@@ -9,22 +9,25 @@ public partial class AHDialogEditOptions
     [Parameter] public ModuleBase Module { get; set; } = default!;
     [CascadingParameter] MudDialogInstance MudDialog { get; set; } = default!;
 
-    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
     [Inject] private IServiceScopeFactory ScopeFactory { get; set; } = default!;
 
     private DynamicComponent? RefOptions { get; set; } = default!;
+    public bool InSaving { get; set; }
 
     private async Task SubmitAsync()
     {
+        InSaving = true;
         await ((ISavable)RefOptions!.Instance!).SaveAsync();
 
         using var scope = ScopeFactory.CreateScope();
         await Module.RefreshOptionsAsync(scope);
 
-        Logger.LogInformation(L["Save options {Module}"], Module.FullInfo);
-        MudDialog.Close(DialogResult.Ok(true));
+        InSaving = false;
 
-        NavigationManager.NavigateTo(NavigationManager.Uri, true);
+        Logger.LogInformation(L["Save options {Module}", Module.FullInfo]);
+        MudDialog.Close(DialogResult.Ok(true));
+        //force refresh
+        //NavigationManager.NavigateTo(NavigationManager.Uri, true);
     }
 
     void Cancel() => MudDialog.Cancel();
